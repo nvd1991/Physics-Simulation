5a19c8c4d33130315eba375fa9c06e17f1359360
Dzung <vietdung@codingdojo.com>
2019-03-24 01:06:58 +0700

    Add simple collision

--- Physics/Vector/readme.txt
+++ Physics/Vector/readme.txt
@@ -1,4 +1,4 @@
-A vector has:
+Vector has:
 - magnitude (the length of the vector)
 - direction (the angle relative to some axis)
 
@@ -8,6 +8,18 @@ Ex Bouncing balls:
     velocity vector,
     moveBall func(),
     checkBounce func(),
-    drawBall func())
-Description: A ball that moves based on it's position and
-velocity, it bounces back if it reaches the border.
+    drawBall func(),
+    checkCollision func())
+Description: 
+1. Balls moves based on it's position and velocity
+2. Balls bounces back if they reaches the border
+3. Simple Collision if 2 balls collide, velocity *= -1
+
+Data:
+balls = [
+        new Ball(createVector(50, 70), createVector(2.5, -2)),
+        new Ball(createVector(233, 300), createVector(1.5, 1)),
+        new Ball(createVector(200, 200), createVector(4.5, -5)),
+        new Ball(createVector(35, 269), createVector(1, -2)),
+        new Ball(createVector(100, 200), createVector(3, 2.5)),
+    ];
--- Physics/Vector/sketch.js
+++ Physics/Vector/sketch.js
@@ -1,50 +1,64 @@
 let balls;
 
-function setup(){
+function setup() {
     createCanvas(400, 400);
     balls = [
-        new Ball(createVector(50, 70), createVector(2.5, -2)),
-        new Ball(createVector(233, 300), createVector(1.5, 1)),
-        new Ball(createVector(200, 200), createVector(4.5, -5)),
-        new Ball(createVector(35, 269), createVector(1, -2)),
-        new Ball(createVector(100, 200), createVector(3, 2.5)),
+        new Ball(10, createVector(50, 70), createVector(2.5, -2)),
+        new Ball(20, createVector(233, 300), createVector(1.5, 1)),
+        new Ball(15, createVector(200, 200), createVector(4.5, -5)),
+        new Ball(30, createVector(35, 269), createVector(1, -2)),
+        new Ball(10, createVector(100, 200), createVector(3, 2.5)),
     ];
 
-    function Ball(location, velocity){
+    function Ball(radius, location, velocity) {
+        this.radius = radius;
         this.location = location;
         this.velocity = velocity;
         this.drawBall = drawBall;
         this.moveBall = moveBall;
         this.checkBounce = checkBounce;
+        this.checkCollision = checkCollision;
     }
 
-    function drawBall(){
-        stroke(255,239,150);
+    function drawBall() {
+        stroke(255, 239, 150);
         strokeWeight(2);
-        fill(107,91,149);
-        circle(this.location.x, this.location.y, 20);
+        fill(107, 91, 149);
+        circle(this.location.x, this.location.y, this.radius);
     }
 
-    function moveBall(){
+    function moveBall() {
         this.location.add(this.velocity);
     }
 
-    function checkBounce(){
-        if(this.location.x > width || this.location.x < 0){
+    function checkBounce() {
+        if (this.location.x > width || this.location.x < 0) {
             this.velocity.x *= -1;
         }
-        if(this.location.y > height || this.location.y < 0){
+        if (this.location.y > height || this.location.y < 0) {
             this.velocity.y *= -1;
+        }
+    }
+
+    function checkCollision() {
+        //Simple collision
+        for (let ball of balls) {
+            if(ball === this) continue;           
+            const distance = this.location.copy().sub(ball.location).mag();
+            if (distance <= (this.radius + ball.radius)) {
+                this.velocity.mult(-1);
+            }
         }
     }
 }
 
-function draw(){
+function draw() {
     background(51, 51, 51);
-    
-    for(let ball of balls){
-        ball.moveBall(); 
+
+    for (let ball of balls) {
+        ball.checkCollision();
         ball.checkBounce();
-        ball.drawBall();           
-    }   
+        ball.moveBall();
+        ball.drawBall();        
+    }
 }
